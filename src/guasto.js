785e8aca808941f0551ab67f5eb52e9fd8d33242
Pigna <andrea_pigna99@hotmail.it>
2021-12-06 17:38:20 +0100

    hide clog

--- src/guasto.js
+++ src/guasto.js
@@ -1,5 +1,5 @@
 exports.guasto = (request, response) => {
-    console.log(request)
+    //console.log(request)
     if(request.query.lavatrice && request.query.text){
         console.log("Invia una email al tecnico riguardo la Lavatrice "+request.query.lavatrice+".\nSegnalazione: "+request.query.text);
         response.send("success!");
