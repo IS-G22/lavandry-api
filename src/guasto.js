ca3d07fb1baaf46e37f5651880c5fc8e9fcc4207
Pigna <andrea_pigna99@hotmail.it>
2021-12-06 17:27:47 +0100

    query

--- src/guasto.js
+++ src/guasto.js
@@ -1,7 +1,7 @@
 exports.guasto = (request, response) => {
     console.log(request)
-    if(request.query.lavatrice && request.quey.text){
-        console.log("Invia una email al tecnico riguardo la Lavatrice "+request.quey.lavatrice+".\nSegnalazione: "+request.quey.text);
+    if(request.query.lavatrice && request.query.text){
+        console.log("Invia una email al tecnico riguardo la Lavatrice "+request.query.lavatrice+".\nSegnalazione: "+request.query.text);
         response.send("success!");
     }else{
         response.send("failure!");
